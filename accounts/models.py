594bbc23f560e705fc8d2588522101cbd8d15216
Caroline Costa <65172953+coraremos@users.noreply.github.com>
2025-12-19 13:18:31 -0300

    feat: implementacao do AbstractUser no models

--- accounts/models.py
+++ accounts/models.py
@@ -1,15 +1,13 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
 class Accounts(models.Model):
     nome = models.CharField(max_length=200, verbose_name='Nome')
     cargo = models.CharField(max_length=200, verbose_name='Cargo')
     empresa = models.CharField(max_length=200, verbose_name='Empresa')
 
-class User(models.Model):
-    is_diretoria = models.BooleanField(default=False, verbose_name='Diretoria')
-    is_associado = models.BooleanField(default=False, verbose_name='Associado')
-    is_afiliado = models.BooleanField(default=False, verbose_name='Afiliado')
+class CustomUser(AbstractUser):
+    pass
 
     def __str__(self):
-        return self.titulo
+        return self.username
