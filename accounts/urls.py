d57b62e73096813a3358dfadae025db675027269
Caroline Costa <65172953+coraremos@users.noreply.github.com>
2025-12-31 16:08:45 -0300

    feat: aplicação do caminho de logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from . import views 
 from .views import Teste
+from django.contrib.auth import logout
+from django.shortcuts import redirect
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
 
 urlpatterns = [
     path('', views.login_view, name='login'),
@@ -18,8 +24,12 @@ urlpatterns = [
 #Usuario de teste    
     path("teste/", Teste, 
          name="usuario_teste"),
-         
+
     path('diretoria/', 
          views.visao_diretoria, 
          name='visao_diretoria'),
+    
+    path('logout/', 
+         logout_view, 
+         name='logout'),
 ]
