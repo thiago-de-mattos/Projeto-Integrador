2dc22a1a55df80dd54fb3f5b99f531f546471db5
Grediness <168847376+PedroDefanti@users.noreply.github.com>
2026-01-03 23:35:36 -0300

    feat:adição da tabela de Empresas e  Projetos

--- accounts/models.py
+++ accounts/models.py
@@ -11,3 +11,58 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+    
+
+class Empresas(models.Model):
+    nome=models.CharField(max_length=150)
+    
+    razao=models.CharField(max_length=150)
+    
+    cnpj=models.CharField(max_length=18,unique=True)
+    
+    email=models.EmailField(unique=True)
+    
+    telefone=models.CharField(max_length=15)
+    
+    site=models.CharField()
+    
+    endereco=models.CharField(max_length=150)
+    
+    cep=models.CharField(max_length=9)
+    
+    numero=models.CharField(max_length=9)
+    
+    complemento=models.CharField(max_length=150)
+    
+    
+    nome_responsavel=models.CharField(max_length=150)
+    
+    razao_responsavel=models.CharField(max_length=150)
+    
+    cpf_responsavel=models.CharField(max_length=18,unique=True)
+    
+    email_responsavel=models.EmailField(unique=True)
+    
+    telefone_responsavel=models.CharField(max_length=15)
+    
+    nick_responsavel=models.CharField()
+    
+    endereco_responsavel=models.CharField(max_length=150)
+    
+    cep_responsavel=models.CharField(max_length=9)
+    
+    numero_responsavel=models.CharField(max_length=9)
+    
+    complemento_responsavel=models.CharField(max_length=150)
+    
+class Projetos(models.Model):
+    
+    nome=models.CharField(max_length=150)
+    
+    descricao=models.TextField(max_length=450)
+    
+    status=models.CharField(max_length=150)
+    
+    equipe=models.CharField(max_length=150)
+    
+    links=models.CharField(max_length=150)
