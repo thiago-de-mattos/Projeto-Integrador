7d55949163d7850167dcccdd2f59a4a582834288
Grediness <168847376+PedroDefanti@users.noreply.github.com>
2026-01-03 23:36:23 -0300

    feat:adição de novas rotas voltas para a empresas e Projetos

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views 
-from .views import Teste
+from .views import Teste_Diretoria
 from django.contrib.auth import logout
 from django.shortcuts import redirect
 
@@ -22,7 +22,7 @@ urlpatterns = [
          views.cadastro,
          name='cadastro'),
 #Usuario de teste    
-    path("teste/", Teste, 
+    path("teste/", Teste_Diretoria, 
          name="usuario_teste"),
 
     path('diretoria/', 
@@ -32,4 +32,15 @@ urlpatterns = [
     path('logout/', 
          logout_view, 
          name='logout'),
+    
+    path('empresas/',
+         views.cadastro_empresas,
+         name='empresas'),
+    
+    path('projetos/',
+         views.cadastro_projetos,
+         name='projetos'),
+    
+    path('empresas/listar',views.listagem_empresas,name='listagem_empresas'),
+    path('empresas/editar/<int:pk>/',views.editar_empresas,name='editar_empresas'),
 ]
