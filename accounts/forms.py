f434fa4f4ba566db622dfe9422ed49bf6155739d
vitordanielrosa <vitorcabral2004@gmail.com>
2026-01-15 01:04:07 -0300

    feat: adicionado EstudioForm com campos para detalhes do estúdio

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.core.exceptions import ValidationError
-from .models import Empresa, Projeto, Profile
+from .models import Empresa, Projeto, Profile, Estudios
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
@@ -36,31 +36,19 @@ class EmpresaForm(forms.ModelForm):
 
 class EstudioForm(forms.ModelForm):
     class Meta:
-        model=Empresa
-        fields="__all__"
-        
+        model = Estudios
+        fields = [
+            'nome_do_estudio',
+            'email',
+            'telefone',
+            'endereco',
+        ]
+
         widgets={
-            'nome_fantasia':forms.TextInput(attrs={'placeholder':'Digite o nome da empresa'}),
-            'razao_social':forms.TextInput(attrs={'placeholder':'Digite a razão social'}),
-            'cnpj':forms.TextInput(attrs={'placeholder':'Digite o CNPJ'}),
+            'nome_do_estudio':forms.TextInput(attrs={'placeholder':'Digite o nome da empresa'}),
             'email':forms.EmailInput(attrs={'placeholder':'Digite o email da empresa'}),
             'telefone':forms.TextInput(attrs={'placeholder':'Digite o telefone da empresa'}),
-            'site':forms.TextInput(attrs={'placeholder':'Digite o site da empresa'}),
-            'endereco_completo':forms.TextInput(attrs={'placeholder':'Digite o endereço da empresa'}),
-            'cep':forms.TextInput(attrs={'placeholder':'Digite o CEP da empresa'}),
-            'cidade':forms.TextInput(attrs={'placeholder':'Digite a cidade da empresa'}),
-            'complemento':forms.TextInput(attrs={'placeholder':'Digite o complemento da empresa'}),
-            
-            'nome_responsavel':forms.TextInput(attrs={'placeholder':'Digite o nome da empresa'}),
-            'razao_responsavel':forms.TextInput(attrs={'placeholder':'Digite a razão social'}),
-            'cpf_responsavel':forms.TextInput(attrs={'placeholder':'Digite o CNPJ'}),
-            'email_responsavel':forms.EmailInput(attrs={'placeholder':'Digite o email da empresa'}),
-            'telefone_responsavel':forms.TextInput(attrs={'placeholder':'Digite o telefone da empresa'}),
-            'nick_responsavel':forms.URLInput(attrs={'placeholder':'Digite o site da empresa'}),
-            'endereco_responsavel':forms.TextInput(attrs={'placeholder':'Digite o endereço da empresa'}),
-            'cep_responsavel':forms.TextInput(attrs={'placeholder':'Digite o CEP da empresa'}),
-            'numero_responsavel':forms.TextInput(attrs={'placeholder':'Digite o número da empresa'}),
-            'complemento_responsavel':forms.TextInput(attrs={'placeholder':'Digite o complemento da empresa'}),
+            'endereco':forms.TextInput(attrs={'placeholder':'Digite o endereco da empresa'}),
             }
         
         
