9b6bd878eb7adaca7c49f84276264afe3dcfadae
thiago-de-mattos <205856258+thiago-de-mattos@users.noreply.github.com>
2026-01-19 11:25:16 -0300

    feat: adiciona autenticação para rolepermissions

--- integrator_project/settings.py
+++ integrator_project/settings.py
@@ -100,6 +100,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTHENTICATION_BACKENDS = [
+    'rolepermissions.backends.RolePermissionsBackend',
+    'django.contrib.auth.backends.ModelBackend',
+]
 
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
