31a109b2cdcf9991b6f82138fb9ce9810a01e680
PedroDefanti <168847376+PedroDefanti@users.noreply.github.com>
2026-01-21 11:32:53 -0300

    feat:adicionando for dos campos

--- accounts/forms.py
+++ accounts/forms.py
@@ -142,21 +142,28 @@ class ResponsavelForm(forms.ModelForm):
 
 class EmpresaForm(forms.ModelForm):
     class Meta:
-        model=Empresa
-        fields="__all__"
+        model = Empresa
+        fields = "__all__"
         
-        widgets={
-            'nome_fantasia':forms.TextInput(attrs={'placeholder':'Digite o nome da empresa'}),
-            'razao_social':forms.TextInput(attrs={'placeholder':'Digite a razão social'}),
-            'cnpj':forms.TextInput(attrs={'placeholder':'Digite o CNPJ'}),
-            'email':forms.EmailInput(attrs={'placeholder':'Digite o email da empresa'}),
-            'telefone':forms.TextInput(attrs={'placeholder':'Digite o telefone da empresa'}),
-            'site':forms.TextInput(attrs={'placeholder':'Digite o site da empresa'}),
-            'endereco_completo':forms.TextInput(attrs={'placeholder':'Digite o endereço da empresa'}),
-            'cep':forms.TextInput(attrs={'placeholder':'Digite o CEP da empresa'}),
-            'cidade':forms.TextInput(attrs={'placeholder':'Digite a cidade da empresa'}),
-            'complemento':forms.TextInput(attrs={'placeholder':'Digite o complemento da empresa'}),
-            }
+        widgets = {
+            'nome_fantasia': forms.TextInput(attrs={'placeholder': 'Digite o nome da empresa'}),
+            'razao_social': forms.TextInput(attrs={'placeholder': 'Digite a razão social'}),
+            'cnpj': forms.TextInput(attrs={'placeholder': 'Digite o CNPJ'}),
+            'email': forms.EmailInput(attrs={'placeholder': 'Digite o email da empresa'}),
+            'telefone': forms.TextInput(attrs={'placeholder': 'Digite o telefone'}),
+            'site': forms.TextInput(attrs={'placeholder': 'Digite o site'}),
+            'endereco_completo': forms.TextInput(attrs={'placeholder': 'Digite o endereço'}),
+            'cep': forms.TextInput(attrs={'placeholder': 'Digite o CEP'}),
+            'cidade': forms.TextInput(attrs={'placeholder': 'Digite a cidade'}),
+            'complemento': forms.TextInput(attrs={'placeholder': 'Digite o complemento'}),
+            'data_fundacao':forms.TextInput(attrs={'placeholder': 'Ex:dd/mm/aaaa'})
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Este loop adiciona automaticamente a classe 'form-input' a todos os campos
+        for field in self.fields.values():
+            field.widget.attrs.update({'class': 'form-input'})
 
 class EstudioForm(forms.ModelForm):
     class Meta:
