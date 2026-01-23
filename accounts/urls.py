6587a1fee3598746d1812f572c7dc0b7882304cd
Caroline Costa <65172953+coraremos@users.noreply.github.com>
2026-01-23 11:56:37 -0300

    fix: remover import inativo para evitar conflito

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from . import views 
-from .views import Teste_Diretoria
 from django.contrib.auth import logout
 from django.shortcuts import redirect
 from django.conf import settings
