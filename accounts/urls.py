8556eb6fd899fdb961dcbbe793b4c832b7f91227
thiago de mattos <205856258+thiago-de-mattos@users.noreply.github.com>
2026-01-23 12:29:01 -0300

    feat: caminho de cadastro

--- accounts/urls.py
+++ accounts/urls.py
@@ -17,6 +17,9 @@ urlpatterns = [
      path('logout/', logout_view, name='logout'),
 
      path('cadastro/', views.cadastro, name='cadastro'),
+     path('cadastro/empresa/', views.cadastro_empresa, name='cadastro_empresa'),
+     path('cadastro/profissional/', views.cadastro_profissional, name='cadastro_profissional'),
+    
      path('perfil/editar/', views.editar_meu_perfil, name='editar_meu_perfil'),
      
      path('diretoria/', views.visao_diretoria, name='visao_diretoria'),
