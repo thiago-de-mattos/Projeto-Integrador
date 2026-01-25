5927f84f58923af1747a23b59c8b9725e63be984
vitordanielrosa <vitorcabral2004@gmail.com>
2026-01-25 17:14:41 -0300

    fix: corrigir filtro de tipo de empresa na função empresas_vitrine

--- accounts/views.py
+++ accounts/views.py
@@ -981,7 +981,7 @@ def empresas_vitrine(request):
     
     # Filtro por tipo
     if tipo != 'Todos':
-        empresas = empresas.filter(tipo=tipo)
+        empresas = empresas.filter(tipo_empresa=tipo)
     
     # Listas para filtros - usando 'municipio'
     cidades = Empresa.objects.values_list('municipio', flat=True).distinct().order_by('municipio')
@@ -996,9 +996,9 @@ def empresas_vitrine(request):
     ]
     
     # Serializar para Alpine.js - usando campos corretos
-    empresas_json = json.dumps(list(empresas.values(
-        'id', 'nome', 'nome_fantasia', 'tipo_empresa', 'municipio', 'porte_empresa', 'associada_acjogos'
-    )), default=str)
+    empresas_json = list(empresas.values(
+        'id','nome','nome_fantasia','tipo_empresa','municipio','porte_empresa','associada_acjogos'
+    ))
     
     context = {
         'empresas': empresas,
