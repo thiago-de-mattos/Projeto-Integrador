49739d23ca591a3ab0c83b8f3c53d2f653b6082b
vitordanielrosa <vitorcabral2004@gmail.com>
2026-01-26 21:53:35 -0300

    feat: adicione FERNET_KEY para criptografia nas configurações

--- integrator_project/settings.py
+++ integrator_project/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+FERNET_KEY = b'DaDObQK5XrDRiAFB-au__3Kd-Jd_D01wQHRf90tzoz0='
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
